3f7f063d561d215ebbdfbc2ad936611b85004800
nikpopo <nikitapopov2002@yandex.ru>
2024-01-19 19:34:37 +0800

    init

---
+++ tests/defind.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Defind } from "../target/types/defind";
+
+describe("defind", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Defind as Program<Defind>;
+
+  const counter = anchor.web3.Keypair.generate()
+
+  it("Is initialized!", async () => {})
+
+  it("Incremented the count", async () => {})
+});
